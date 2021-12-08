6a4d62a6bdc0eadd2e53d5d3b5772b1f40f65737
Sameer Doshi <sadoshi@microsoft.com>
2021-12-08 15:23:46 -0600

    Enabling getPatientById

--- Graphir.API/Schema/PatientQuery.cs
+++ Graphir.API/Schema/PatientQuery.cs
@@ -1,5 +1,12 @@
-﻿using HotChocolate.Types;
+﻿using Graphir.API.Services;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using HotChocolate.Types;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Web;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Graphir.API.Schema
@@ -9,22 +16,73 @@ namespace Graphir.API.Schema
 
     public class PatientQuery 
     {
+        private readonly ITokenAcquisition _token;
+        private readonly FhirClient _fhirService;
+        private readonly FhirDataConnection _options;
+
+        public PatientQuery(ITokenAcquisition token, FhirClient fhirService, IOptions<FhirDataConnection> options)
+        {
+            _token = token;
+            _fhirService = fhirService;
+            _options = options.Value;
+        }
+
+        
         public async Task<string> GetAllPatients()
         {
             var ret = "patient";
             return JsonConvert.SerializeObject(ret);
         }
 
-        public async Task<string> GetPatientByID(string id)
+        public async Task<Patient> GetPatientByID(string id)
         {
-            var ret = "patient";
-            return JsonConvert.SerializeObject(ret);
+
+            var tPatient = new Patient() { Id = id };
+            var ret = await SearchPatient(tPatient);
+            return ret.FirstOrDefault<Patient>();
         }
+
+        
 
         public async Task<string> GetPatientByName(string id)
         {
             var ret = "patient";
             return JsonConvert.SerializeObject(ret);
         }
+
+       
+        private async Task<IList<Patient>> SearchPatient(Patient Patient)
+        {
+            string givenName = ""; //The given name is not working on the mapping
+            string familyName = Patient.Name[0].Family;
+            string identifier = Patient.Identifier[0].Value;
+            Bundle bundle; if (!string.IsNullOrEmpty(identifier))
+            {
+                bundle = await _fhirService.SearchByIdAsync<Patient>(identifier); if (bundle != null)
+                    return bundle.Entry.Select(p => (Patient)p.Resource).ToList();
+            }
+            if (!string.IsNullOrEmpty(familyName))
+            {
+                bundle = await _fhirService.SearchAsync<Patient>(criteria: new[] { $"family:contains={familyName}" }); if (bundle != null)
+                    return bundle.Entry.Select(p => (Patient)p.Resource).ToList();
+            }
+            return await GetPatientsAsync();
+        }
+
+      
+        private async Task<IList<Patient>> GetPatientsAsync()
+        {
+            var bundle = await _fhirService.SearchAsync<Patient>(pageSize: 50);
+            var result = new List<Patient>();
+            while (bundle != null)
+            {
+                result.AddRange(bundle.Entry.Select(p => (Patient)p.Resource).ToList());
+                bundle = await _fhirService.ContinueAsync(bundle);
+            }
+            return result;
+        }
+
+
+
     }
 }
