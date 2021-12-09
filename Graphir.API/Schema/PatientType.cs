d9bbf0ee10d6e2b1e747b6e3b3c79391cd9d44a7
Phil Jirsa <phil.jirsa@microsoft.com>
2021-12-09 16:54:06 -0600

    update patient query schema for compliance

--- Graphir.API/Schema/PatientType.cs
+++ Graphir.API/Schema/PatientType.cs
@@ -15,17 +15,16 @@ namespace Graphir.API.Schema
             descriptor.Field(p => p.Active);
             descriptor.Field(p => p.Name);
             descriptor.Field(p => p.Language);
-
-            descriptor
-                .Field(p => p.Gender)
-                .Name("gender");
-
+            descriptor.Field(p => p.Gender);
             descriptor.Field(p => p.BirthDate);
             descriptor.Field(p => p.Telecom);
             descriptor.Field(p => p.Address);
             descriptor.Field(p => p.MaritalStatus);
             descriptor.Field(p => p.Photo);
-
+            //descriptor.Field(p => p.Contact); #TODO: use resolver to get related resource
+            descriptor.Field(p => p.Communication);
+            //descriptor.Field(p => p.GeneralPractitioner); #TODO: use resolver to get related resource
+            //descriptor.Field(p => p.ManagingOrganization); #TODO: use resolver to get related resource
 
             /*
             implicitRules: uri _implicitRules: ElementBase
@@ -34,13 +33,36 @@ namespace Graphir.API.Schema
                  deceasedDateTime: dateTime _deceasedDateTime: ElementBase
                     multipleBirthBoolean: Boolean _multipleBirthBoolean: ElementBase
                    multipleBirthInteger: Int _multipleBirthInteger: ElementBase
-                  photo: [Attachment]
-                    contact:[PatientContact]
-                    communication:[PatientCommunication]
-                    generalPractitioner:[Reference]
-                    managingOrganization: Reference
                     link: [PatientLink]
             */
+        }
+    }
+
+    public class PatientContactType : ObjectType<Patient.ContactComponent>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Patient.ContactComponent> descriptor)
+        {
+            descriptor.BindFieldsExplicitly();
+
+            descriptor.Field(c => c.Address);
+            descriptor.Field(c => c.Gender);
+            descriptor.Field(c => c.Name);
+            descriptor.Field(c => c.Organization.Url)
+                .Name("organization");
+            descriptor.Field(c => c.Period);
+            descriptor.Field(c => c.Relationship);
+            descriptor.Field(c => c.Telecom);
+        }
+    }
+
+    public class PatientCommunicationType : ObjectType<Patient.CommunicationComponent>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Patient.CommunicationComponent> descriptor)
+        {
+            descriptor.BindFieldsExplicitly();
+
+            descriptor.Field(c => c.Language);
+            descriptor.Field(c => c.Preferred);
         }
     }
 
