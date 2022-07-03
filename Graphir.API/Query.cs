927fcd2302d1a0487fe36eb72a82c1fb6a8fcfa6
MFM_Fazrin <mmfazrin@phcc.gov.qa>
2022-07-03 11:50:10 +0300

    Project updated From .NET 5 to .NET 6 and related NuGet packages updated and tested, some unused using statements removed and GetMe Method Optimised with Nullable String checks

--- Graphir.API/Query.cs
+++ Graphir.API/Query.cs
@@ -1,17 +1,7 @@
-﻿using Graphir.API.Services;
-using Hl7.Fhir.Rest;
-using Hl7.Fhir.Model;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Identity.Web;
+﻿using Hl7.Fhir.Rest;
 using Newtonsoft.Json;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.Http.Headers;
-using Microsoft.Extensions.Options;
-using static Hl7.Fhir.Model.Bundle;
-using System.Linq;
 
 namespace Graphir.API
 {
@@ -31,9 +21,9 @@ namespace Graphir.API
             return JsonConvert.SerializeObject(meta);
         }
 
-        public string GetMe(ClaimsPrincipal principal)
+        public string? GetMe(ClaimsPrincipal principal)
         {
-            return principal.Identity.Name;
+            return principal.Identity?.Name;
         }               
 
     }    
