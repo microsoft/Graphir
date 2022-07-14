88e2b02a70769ed906d6d4109d91f791f596541b
Fazrin <mfmfazrin1986@gmail.com>
2022-07-14 22:00:13 +0300

    Global usings organized with regions directives

--- Graphir.API/Program.cs
+++ Graphir.API/Program.cs
@@ -1,38 +1,52 @@
-﻿global using System.Security.Claims;
+﻿#region System
+global using System;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Threading;
 global using System.Threading.Tasks;
+global using System.Security.Claims;
+global using System.Net.Http.Headers;
+global using System.Collections.Generic;
+#endregion
+
+#region Microsoft
+global using Microsoft.Identity.Web;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.Extensions.Hosting;
-global using Newtonsoft.Json;
-global using Hl7.Fhir.Rest;
-global using Hl7.Fhir.Model;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+#endregion
+
+#region Grahpir
 global using Graphir.API.Extensions;
 global using Graphir.API.Patients;
 global using Graphir.API.Practitioners;
 global using Graphir.API.Services;
-global using Microsoft.AspNetCore.Builder;
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Hosting;
-global using Microsoft.Identity.Web;
-global using GreenDonut;
-global using System.Collections.Generic;
 global using Graphir.API.Schema;
-
-global using System;
-global using System.Linq;
-global using HotChocolate.Types.Pagination;
 global using Graphir.API.Utils;
+#endregion
 
+#region Hotchocolate
 global using HotChocolate;
+global using HotChocolate.Types;
 global using HotChocolate.Execution;
 global using HotChocolate.Execution.Instrumentation;
-global using Microsoft.Extensions.Logging;
-
-global using System.Net.Http;
-global using System.Net.Http.Headers;
-global using System.Threading;
+global using HotChocolate.Types.Pagination;
 global using HotChocolate.Execution.Configuration;
-global using HotChocolate.Types;
+#endregion
+
+#region Hl7
+global using Hl7.Fhir.Rest;
+global using Hl7.Fhir.Model;
+#endregion
+
+#region Others
+global using Newtonsoft.Json;
+global using GreenDonut;
+#endregion
+
 
 namespace Graphir.API
 {
