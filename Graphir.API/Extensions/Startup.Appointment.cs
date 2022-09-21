c45e87e61277b02d9659fe1e6f772dcabbe695a3
Fazrin <mfmfazrin1986@gmail.com>
2022-09-21 21:40:28 +0300

    Appointment Resource and Queries Added

---
+++ Graphir.API/Extensions/Startup.Appointment.cs
@@ -0,0 +1,14 @@
+﻿using Graphir.API.Schema;
+using HotChocolate.Execution.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Graphir.API.Extensions;
+
+internal static class AppointmentStartup
+{
+    public static IRequestExecutorBuilder AddAppointment(this IRequestExecutorBuilder graphBuilder)
+    {
+        return graphBuilder.AddType<AppointmentType>();
+    }
+    
+}
