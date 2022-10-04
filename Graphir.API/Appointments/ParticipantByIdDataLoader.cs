aff19d8f29a688e3227bbca3db6b5992f33cc01d
mjschanne <mjschanne@microsoft.com>
2022-10-04 12:07:57 -0400

    Add WIP changes

---
+++ Graphir.API/Appointments/ParticipantByIdDataLoader.cs
@@ -0,0 +1,30 @@
+﻿using GreenDonut;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Graphir.API.Appointments
+{
+    // todo: create Appointmen
+    public class ParticipantByIdDataLoader : BatchDataLoader<string, BackboneElement>
+    {
+        private readonly FhirClient _fhirService;
+
+        public ParticipantByIdDataLoader(
+            IBatchScheduler scheduler,
+            FhirClient fhirService,
+            DataLoaderOptions options)
+            : base(scheduler, options)
+        {
+            _fhirService = fhirService;
+        }
+
+        protected override async Task<IReadOnlyDictionary<string, BackboneElement>> LoadBatchAsync(IReadOnlyList<string> keys, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Graphir.API/Schema/AppointmentType.cs
+++ Graphir.API/Schema/AppointmentType.cs
@@ -1,5 +1,11 @@
-﻿using Hl7.Fhir.Model;
+﻿using Graphir.API.Practitioners;
+using Hl7.Fhir.Model;
+using HotChocolate;
 using HotChocolate.Types;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace Graphir.API.Schema;
 
@@ -32,6 +38,113 @@ public class AppointmentType : ObjectType<Appointment>
         //TODO: need to add participant resolvers 
         //descriptor.Field(x => x.Participant);
     }
+
+    //private class PatientResolvers
+    //{
+    //    public async Task<IReadOnlyList<Practitioner>> GetApppointmentAsync(
+    //        [Parent] Appointment patient,
+    //        PractitionerByIdDataLoader practitionerById,
+    //        CancellationToken cancellationToken
+    //    )
+    //    {
+    //        var refs = patient.GeneralPractitioner.Select(p => p.Reference.Split('/').LastOrDefault());
+
+    //        var results = await practitionerById.LoadAsync(refs.ToArray(), cancellationToken);
+
+    //        return results;
+    //    }
+    //}
+}
+
+// type AppointmentParticipant {
+public class AppointmentParticipantType : ObjectType<Appointment.ParticipantComponent> // ???
+{
+
+    protected override void Configure(IObjectTypeDescriptor<Appointment.ParticipantComponent> descriptor)
+    {
+        descriptor.BindFieldsExplicitly();
+        descriptor.Field(x => x.ElementId).Type<NonNullType<IdType>>();
+        descriptor.Field(x => x.Extension).Type<ExtensionType>();
+        descriptor.Field(x => x.ModifierExtension).Type<ExtensionType>();
+        descriptor.Field(x => x.Type).Type<CodeableConceptType>();
+        descriptor.Field(x => x.Period).Type<PeriodType>();
+        descriptor.Field(x => x.Status).Type<CodeType>();
+
+        // WIP, commenting out for now to preserve compilation
+        // actor: Reference
+        //descriptor.Field(x => x.Actor).Type<ActorType>();
+        //descriptor.Field(x => x.Required).Type<BooleanType>();
+        // status: code  _status: ElementBase
+    }
+}
+
+// todo: finish implementing
+public class ActorType // : ObjectType<> // ???
+{
+
+    //protected override void Configure(IObjectTypeDescriptor<> descriptor)
+    //{
+    //    descriptor.BindFieldsExplicitly();
+
+    //    // todo: add other descriptors
+    //}
+
+    /*
+     
+            // Summary:
+            //     Person, Location/HealthcareService or Device
+            [FhirElement("actor", InSummary = true, Order = 50)]
+            [CLSCompliant(false)]
+            [References(new string[] { "Patient", "Practitioner", "PractitionerRole", "RelatedPerson", "Device", "HealthcareService", "Location" })]
+            [DataMember]
+            public ResourceReference Actor
+            {
+                get
+                {
+                    return _Actor;
+                }
+                set
+                {
+                    _Actor = value;
+                    OnPropertyChanged("Actor");
+                }
+            }
+     
+     */
 }
 
 
+// todo: finish implementing
+public class CodeType : ObjectType<Code<ParticipationStatus>> // ???
+{
+
+    protected override void Configure(IObjectTypeDescriptor<Code<ParticipationStatus>> descriptor)
+    {
+        descriptor.BindFieldsExplicitly();
+
+        // todo: add other descriptors
+    }
+
+    /*
+     
+           // Summary:
+            //     accepted | declined | tentative | needs-action
+            [FhirElement("status", InSummary = true, Order = 70)]
+            [DeclaredType(Type = typeof(Code))]
+            [Cardinality(Min = 1, Max = 1)]
+            [DataMember]
+            public Code<ParticipationStatus> StatusElement
+            {
+                get
+                {
+                    return _StatusElement;
+                }
+                set
+                {
+                    _StatusElement = value;
+                    OnPropertyChanged("StatusElement");
+                }
+            }
+     
+     */
+}
