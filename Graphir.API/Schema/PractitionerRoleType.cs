002fbb761405a78ac989f16b35e5aa86784fca57
Phil Jirsa <phil.jirsa@microsoft.com>
2022-10-31 14:16:46 -0500

    adds implementation of PractitionerRole

--- Graphir.API/Schema/PractitionerRoleType.cs
+++ Graphir.API/Schema/PractitionerRoleType.cs
@@ -5,11 +5,107 @@ namespace Graphir.API.Schema;
 
 public class PractitionerRoleType : ObjectType<PractitionerRole>
 {
-    // TODO: finish PractitionerRole
     protected override void Configure(IObjectTypeDescriptor<PractitionerRole> descriptor)
     {
         descriptor.BindFieldsExplicitly();
 
         descriptor.Field(p => p.Id);
+        descriptor.Field(p => p.Meta);
+        descriptor.Field(p => p.Language);
+        descriptor.Field(p => p.Text);
+        descriptor.Field(p => p.Extension);
+        descriptor.Field(p => p.ModifierExtension);
+        descriptor.Field(p => p.Identifier);
+        descriptor.Field(p => p.Active);
+        descriptor.Field(p => p.Period);
+        descriptor.Field(p => p.Practitioner).Type<ResourceReferenceType<PractitionerReferenceType>>();
+        descriptor.Field(p => p.Organization).Type<ResourceReferenceType<OrganizationReferenceType>>();
+        descriptor.Field(p => p.Code);
+        descriptor.Field(p => p.Specialty);
+        descriptor.Field(p => p.Location).Type<ListType<ResourceReferenceType<LocationReferenceType>>>();
+        descriptor.Field(p => p.HealthcareService).Type<ListType<ResourceReferenceType<HealthcareServiceReferenceType>>>();
+        descriptor.Field(p => p.Telecom);
+        descriptor.Field(p => p.AvailableTime).Type<PractitionerRoleAvailableTimeType>();
+        descriptor.Field(p => p.NotAvailable).Type<PractitionerRoleNotAvailableType>();
+        descriptor.Field(p => p.AvailabilityExceptions);
+        descriptor.Field(p => p.Endpoint).Type<ListType<ResourceReferenceType<PractitionerRoleEndpointReferenceType>>>();
+    }
+
+    private class PractitionerReferenceType : UnionType
+    {
+        protected override void Configure(IUnionTypeDescriptor descriptor)
+        {
+            descriptor.Name("PractitionerRolePractitionerReference");
+            descriptor.Description("Reference(Practitioner)");
+            descriptor.Type<PractitionerType>();
+        }
+    }
+
+    private class OrganizationReferenceType : UnionType
+    {
+        protected override void Configure(IUnionTypeDescriptor descriptor)
+        {
+            descriptor.Name("PractitionerRoleOrganizationReference");
+            descriptor.Description("Reference(Organization)");
+            descriptor.Type<OrganizationType>();
+        }
+    }
+
+    private class LocationReferenceType : UnionType
+    {
+        protected override void Configure(IUnionTypeDescriptor descriptor)
+        {
+            descriptor.Name("PractitionerRoleLocationReference");
+            descriptor.Description("Reference(Location)");
+            descriptor.Type<LocationType>();
+        }
+    }
+
+    private class HealthcareServiceReferenceType : UnionType
+    {
+        protected override void Configure(IUnionTypeDescriptor descriptor)
+        {
+            descriptor.Name("PractitionerRoleHealthcareServiceReference");
+            descriptor.Description("Reference(HealthcareService)");
+            descriptor.Type<HealthcareServiceType>();
+        }
+    }
+
+    private class PractitionerRoleAvailableTimeType : ObjectType<PractitionerRole.AvailableTimeComponent>
+    {
+        protected override void Configure(IObjectTypeDescriptor<PractitionerRole.AvailableTimeComponent> descriptor)
+        {
+            descriptor.Name("PractitionerRoleAvailableTime");
+            descriptor.BindFieldsExplicitly();
+            descriptor.Field(x => x.Extension);
+            descriptor.Field(x => x.ModifierExtension);
+            descriptor.Field(x => x.DaysOfWeek);
+            descriptor.Field(x => x.AllDay);
+            descriptor.Field(x => x.AvailableStartTime);
+            descriptor.Field(x => x.AvailableEndTime);
+        }
+    }
+
+    private class PractitionerRoleNotAvailableType : ObjectType<PractitionerRole.NotAvailableComponent>
+    {
+        protected override void Configure(IObjectTypeDescriptor<PractitionerRole.NotAvailableComponent> descriptor)
+        {
+            descriptor.Name("PractitionerRoleNotAvailable");
+            descriptor.BindFieldsExplicitly();
+            descriptor.Field(x => x.Extension);
+            descriptor.Field(x => x.ModifierExtension);
+            descriptor.Field(x => x.Description);
+            descriptor.Field(x => x.During);
+        }
+    }
+
+    private class PractitionerRoleEndpointReferenceType : UnionType
+    {
+        protected override void Configure(IUnionTypeDescriptor descriptor)
+        {
+            descriptor.Name("PractitionerRoleEndpointReference");
+            descriptor.Description("Reference(Endpoint)");
+            descriptor.Type<EndpointType>();
+        }
     }
 }
