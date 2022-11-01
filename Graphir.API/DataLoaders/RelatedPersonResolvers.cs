4ef8b2b871bb14a2b9f6126b90b3bc061ae261f5
Phil Jirsa <phil.jirsa@microsoft.com>
2022-10-31 19:43:24 -0500

    use fhirjsonclient for relatedperson resolver

---
+++ Graphir.API/DataLoaders/RelatedPersonResolvers.cs
@@ -0,0 +1,30 @@
+﻿using Graphir.API.Services;
+using Hl7.Fhir.Model;
+using HotChocolate;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Graphir.API.DataLoaders;
+
+public class RelatedPersonResolvers
+{
+    public async Task<List<RelatedPerson>> GetRelatedPersonResources(
+        [Service] FhirJsonClient client,
+        [Argument("family")] string? lastName,
+        [Argument("given")] string? firstName,
+        [Argument("birthdate")] DateTime? birthDate)
+    {
+        var criteria = new List<string>();
+
+        if (!string.IsNullOrEmpty(lastName))
+            criteria.Add($"family={lastName}");
+        if (!string.IsNullOrEmpty(firstName))
+            criteria.Add($"given={firstName}");
+        if (birthDate is not null)
+            criteria.Add($"birthdate={birthDate:yyyy-MM-dd}");
+
+        var result = await client.SearchAsync<RelatedPerson>(criteria);
+        return result;
+    }
+}
