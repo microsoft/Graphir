318283e1347e5b4cbd853c06cb35f81394a206d0
Phil Jirsa <phil.jirsa@microsoft.com>
2022-11-04 12:21:22 -0500

    attempting to use interface for component types

---
+++ Graphir.API/Schema/ComponentType.cs
@@ -0,0 +1,17 @@
+﻿using Hl7.Fhir.Model;
+using HotChocolate.Types;
+
+namespace Graphir.API.Schema
+{
+    public class ComponentType : InterfaceType<BackboneElement>
+    {
+        protected override void Configure(IInterfaceTypeDescriptor<BackboneElement> descriptor)
+        {
+            descriptor.BindFieldsExplicitly();
+            descriptor.Field(x => x.ElementId);
+            descriptor.Field(x => x.Extension);
+            descriptor.Field(x => x.ModifierExtension);
+        }
+    }
+
+}
